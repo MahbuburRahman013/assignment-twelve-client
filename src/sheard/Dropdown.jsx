942f2e55489ce84dd48ac1796bfd005695cd241e
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-11-30 14:52:10 +0600

    added somes icons

--- src/sheard/Dropdown.jsx
+++ src/sheard/Dropdown.jsx
@@ -1,34 +1,44 @@
 import { useContext } from "react";
 import { useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ContextProvider } from "../auth/AuthProvider";
 import toast from 'react-hot-toast';
+import { IoLogOutOutline } from "react-icons/io5";
+import { MdDashboard } from "react-icons/md";
+import { FaUserAlt } from "react-icons/fa";
 
-const Dropdown = ({marTop, mar}) => {
+const Dropdown = ({ marTop, mar }) => {
     const [isShow, setIsShow] = useState(false);
-    const {logOutUser , user} = useContext(ContextProvider);
+    const { logOutUser, user } = useContext(ContextProvider);
 
 
 
-const handleLogout = () => {
-     logOutUser()
-     .then(()=>{
-          toast.success('sign out successfully')
-     })
-}
+    const handleLogout = () => {
+        logOutUser()
+            .then(() => {
+                toast.success('sign out successfully')
+            })
+    }
 
     return (
         <div>
             <div className="cursor-pointer">
-                <img onClick={()=> setIsShow(!isShow)} className="w-8 h-8 border-2 border-blue-500 rounded-full" src={user?.photoURL} alt="" />
-            
+                <img onClick={() => setIsShow(!isShow)} className="w-8 h-8 border-2 border-blue-500 rounded-full" src={user?.photoURL} alt="" />
+
             </div>
             {
                 isShow && <div className={`shadow-lg rounded-md z-10 bg-white absolute px-8 py-2  ${mar} ${marTop}`}>
-                    <div className="py-1 font-semibold">{user?.displayName}</div>
-                    <div className="py-1 font-bold"><Link to='/dashboard'>Dashboard</Link></div>
-                    <div className="py-1 font-bold">
-                        <button onClick={handleLogout}>Logout</button>
+                    <div className="py-1 lg:font-semibold flex gap-1 items-center"><FaUserAlt></FaUserAlt><p>{user?.displayName}</p></div>
+
+                    <div className="py-1 lg:font-bold flex gap-1 items-center">
+                        <Link to='/dashboard'>
+                        <button className="flex gap-1 items-center"><MdDashboard></MdDashboard>Dashboard</button>
+                        </Link>
+                        
+                    </div>
+
+                    <div className="py-1 lg:font-bold">
+                        <button className="flex gap-1 items-center" onClick={handleLogout}><IoLogOutOutline></IoLogOutOutline>Logout</button>
                     </div>
                 </div>
             }
